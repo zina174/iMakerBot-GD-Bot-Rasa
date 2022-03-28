72d4c3af5900e46635e41b22844e2ec1e1964354
mohamed souid <souidmohamed1991@gmail.com>
2022-03-28 16:24:12 +0100

    restapi

--- flask_api.py
+++ flask_api.py
@@ -1,8 +1,6 @@
+
 import requests
 from flask import Flask, request, jsonify
-import flask_restful
-
-
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,14 +8,10 @@ CORS(app)
 app.config['DEBUG'] = True
 app.config['JSON_SORT_KEYS'] = False
 
-# Creating appi instance
-api = flask_restful.Api(app)
+@app.route('/chat', methods=['GET','POST'])
+def rasa():
 
-class rasa(flask_restful.Resource):
-    def get(self):
-        return jsonify({"message":"Make a post request for result"})
-
-    def post(self):
+    if request.method == 'POST':
         id = request.get_json()['id']
         msg = request.get_json()['msg']
 
@@ -25,15 +19,9 @@ class rasa(flask_restful.Resource):
 
         data = {'sender':id, 'message':msg}
 
-        result = requests.post(url, json=data)
-
-        # print("*"*40)
-        print(result.json())
-        # print("*"*40)
+        result = requests.post(url=url, json = data)
 
         return jsonify(result.json())
-api.add_resource(rasa,'/chat')
 
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run()
